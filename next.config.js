b22eccd10a1dc29751c519be2ee4a52f2a1ed163
jnghg <eniows@naver.com>
2022-09-18 18:03:16 +0900

    init

--- next.config.js
+++ next.config.js
@@ -1,7 +1,49 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const deps = require("./package.json").dependencies;
+const Dotenv = require("dotenv-webpack");
+const path = require("path");
 
-module.exports = nextConfig
+module.exports = {
+  trailingSlash: true,
+  webpack5: true,
+  webpack(config, options) {
+    const { webpack } = options;
+
+    config.module.rules.push({
+      test: /_app.tsx/,
+      loader: "@module-federation/nextjs-mf/lib/federation-loader.js",
+    });
+
+    config.output.publicPath = process.env.FRONT_HOST;
+    config.plugins.push(
+      new webpack.container.ModuleFederationPlugin({
+        remoteType: "var",
+        remotes: {
+          app1: "app1",
+          app2: "app2",
+        },
+        shared: {
+          react: {
+            eager: true,
+            requiredVersion: deps["react"],
+            singleton: true,
+          },
+          "react-dom": {
+            eager: true,
+            requiredVersion: deps["react-dom"],
+            singleton: true,
+          },
+
+          "mobx-react": {
+            eager: true,
+            requiredVersion: deps["mobx-react"],
+            singleton: true,
+          },
+        },
+      }),
+      new Dotenv({
+        path: path.resolve(__dirname, "config", `.env.${process.env.APP_ENV}`),
+      })
+    );
+    return config;
+  },
+};
